812168222ec81d45698fad5e990bd423e5a38010
omkar09 <153846657+omphapale@users.noreply.github.com>
2025-07-06 10:20:12 +0530

    Create hotel.java

---
+++ hotel.java
@@ -0,0 +1,10 @@
+package com.example.travel;
+
+public class Hotel {
+    private String name;
+    private String location;
+    private double rating;
+    private double pricePerNight;
+
+    // Constructors, getters, setters omitted for brevity
+}
